d7360aa1b9e6d640e660159c72f283d4a36fcc56
iamenochchirima <iamenochchirima@gmail.com>
2022-11-05 09:49:43 +0200

    Configured mysql and added securty on sensetive data

--- social/social/settings.py
+++ social/social/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vceb$1xry#1swc^^z*9&4)z)9g8fi9k@=-82!#49y%qi7ghnqm'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'social.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': config("DB_NAME"),
+		'USER': config("DB_USER"),
+		'PASSWORD': config("DB_PASSWORD"),
+		'HOST':config("DB_HOST"),
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
