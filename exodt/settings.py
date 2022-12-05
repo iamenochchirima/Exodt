5a13bc41ea2ea463a0fc28ea02eb5573260502b5
iamenochchirima <iamenochchirima@gmail.com>
2022-12-05 09:20:41 +0200

    db pass

--- exodt/settings.py
+++ exodt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'social',
 		'USER': 'root',
-		'PASSWORD': config("DB_PASSWORD"),
+		'DB_PASSWORD': config("DB_PASSWORD"),
 		'HOST': '127.0.0.1',
         'PORT': '3306',
 	}
