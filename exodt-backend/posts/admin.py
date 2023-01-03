45e2e6a4a8a72b15b6de7be420b84d19ed4124d8
iamenochchirima <iamenochchirima@gmail.com>
2023-01-03 23:28:41 +0200

    Updated the slug field

--- exodt-backend/posts/admin.py
+++ exodt-backend/posts/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Post, Like, Comment, Category
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'status']
+    list_filter = ['status']
+    search_fields = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+    # Add this line
+    auto_populate_fields = ['slug']
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Category)
