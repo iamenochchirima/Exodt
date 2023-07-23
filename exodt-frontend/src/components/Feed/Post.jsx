0c72c9865a039971a871d78b9a41baee2025c3e3
iamenochchirima <iamenochchirima@gmail.com>
2023-07-23 21:40:28 +0200

    Fixed like bug

--- exodt-frontend/src/components/Feed/Post.jsx
+++ exodt-frontend/src/components/Feed/Post.jsx
@@ -14,8 +14,7 @@ import { useSelector } from "react-redux";
 const Post = ({ post, theme }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const characterLimit = 100;
-
-  const [displayedPost, setDisplayedPost] = useState(post)
+  const [likes, setLikes] = useState(post.num_likes)
 
   const [likePost, { data: likeRes, error }] = useLikePostMutation();
   const [unlikePost, { data: unlikeRes, error: unlikeError }] = useUnlikePostMutation();
@@ -28,32 +27,39 @@ const Post = ({ post, theme }) => {
   };
 
   useEffect(() => {
-    if (displayedPost.liked_by.includes(profileInfo.id)) {
+    if (post.liked_by.includes(profileInfo.id)) {
       setLiked(true);
     } else {
       setLiked(false);
     }
-  }, [displayedPost, profileInfo]);
+  }, [post, profileInfo]);
 
   const likeUnlike = (id) => {
     const body = {
       "post_id": id
-    }
+    };
     if (liked) {
       try {
-        unlikePost(body);
-        setLiked(false);
-        setDisplayedPost((prevPost) => ({ ...prevPost, num_likes: prevPost.num_likes - 1 }));
+        // Send the unlike request
+        unlikePost(body).unwrap().then(() => {
+          setLiked(false);
+          setLikes(likes - 1);
+        }).catch((error) => {
+          console.log("Failed to unlike post", error);
+        });
       } catch (error) {
-        console.log("Failed to unlike displayedPost", error);
+        console.log("Failed to unlike post", error);
       }
     } else if (!liked) {
       try {
-        likePost(body);
-        setLiked(true);
-        setDisplayedPost((prevPost) => ({ ...prevPost, num_likes: prevPost.num_likes + 1 }));
+        likePost(body).unwrap().then(() => {
+          setLiked(true);
+          setLikes(likes + 1);
+        }).catch((error) => {
+          console.log("Failed to like post", error);
+        });
       } catch (error) {
-        console.log("Failed to like displayedPost", error);
+        console.log("Failed to like post", error);
       }
     }
   };
@@ -65,7 +71,7 @@ const Post = ({ post, theme }) => {
           <div className=" h-[35px] w-[35px] rounded-full">
             <Image
               className="rounded-full"
-              src={displayedPost.profile_image}
+              src={post.profile_image}
               height={55}
               width={55}
               sizes="(max-width: 768px) 100vw"
@@ -73,10 +79,10 @@ const Post = ({ post, theme }) => {
             />
           </div>
 
-          <Link href={`/${encodeURIComponent(displayedPost.username)}/`}>
-            {displayedPost.username}
+          <Link href={`/${encodeURIComponent(post.username)}/`}>
+            {post.username}
           </Link>
-          <h3> {format(displayedPost.created)}</h3>
+          <h3> {format(post.created)}</h3>
         </div>
         <Menu>
           <Menu.Button>
@@ -92,18 +98,16 @@ const Post = ({ post, theme }) => {
             leaveTo="transform opacity-0 scale-95"
           >
             <Menu.Items
-              className={` ${
-                theme === "light" ? `bg-white` : `bg-gray-500`
-              } absolute right-0 botton-10 z-10 mt-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+              className={` ${theme === "light" ? `bg-white` : `bg-gray-500`
+                } absolute right-0 botton-10 z-10 mt-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
             >
               <div className="py-1">
                 <Menu.Item>
                   <button
-                    className={` ${
-                      theme === "light"
-                        ? `text-gray-800 hover:bg-gray-200`
-                        : `text-white hover:bg-gray-700`
-                    } block px-4 py-2 text-sm`}
+                    className={` ${theme === "light"
+                      ? `text-gray-800 hover:bg-gray-200`
+                      : `text-white hover:bg-gray-700`
+                      } block px-4 py-2 text-sm`}
                   >
                     Edit
                   </button>
@@ -114,17 +118,17 @@ const Post = ({ post, theme }) => {
         </Menu>
       </div>
       <div className="flex justify-center">
-        {displayedPost.image && (
-          <Image src={displayedPost.image} alt="displayedPost image" height={200} width={200} />
+        {post.image && (
+          <Image src={post.image} alt="post image" height={200} width={200} />
         )}
       </div>
       <div className="pt-3">
         <p className="font-robotoLight">
           {isExpanded
-            ? displayedPost.content
-            : `${displayedPost.content.slice(0, characterLimit)}...`}
+            ? post.content
+            : `${post.content.slice(0, characterLimit)}...`}
         </p>
-        {!isExpanded && displayedPost.content.length > characterLimit && (
+        {!isExpanded && post.content.length > characterLimit && (
           <button onClick={toggleExpand} className="read-more-button">
             Read more
           </button>
@@ -132,15 +136,15 @@ const Post = ({ post, theme }) => {
       </div>
       <div className="flex gap-3 items-center border-t mt-3 p-3">
         <button
-          onClick={() => likeUnlike(displayedPost.id)}
+          onClick={() => likeUnlike(post.id)}
           className="flex items-center gap-2"
         >
           {liked ? <AiFillHeart size={20} /> : <AiOutlineHeart size={20} />}
-          <span>{displayedPost.num_likes}</span>
+          <span>{likes}</span>
         </button>
         <button className="flex items-center gap-2">
           <AiOutlineComment size={20} />
-          <span>{displayedPost.num_comments}</span>
+          <span>{post.num_comments}</span>
         </button>
       </div>
     </div>
